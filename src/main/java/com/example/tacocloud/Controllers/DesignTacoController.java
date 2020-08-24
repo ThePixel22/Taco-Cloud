0551e48a810fe85e3cbe64d93e7a76dcd1578c56
hungnh <Window control About hungnh@oneempower.com>
2020-08-24 16:05:19 +0700

    3.1 work with JDBC

--- src/main/java/com/example/tacocloud/Controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/Controllers/DesignTacoController.java
@@ -2,7 +2,9 @@ package com.example.tacocloud.Controllers;
 
 
 
+import com.example.tacocloud.Data.IngredientRepository;
 import com.example.tacocloud.Models.Taco;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,36 +29,32 @@ import com.example.tacocloud.Models.Ingredient;
 @RequestMapping("/design")
 public class DesignTacoController {
 
-    @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+    private final IngredientRepository ingredientRepository;
 
+    @Autowired
+    public  DesignTacoController (IngredientRepository ingredientRepository){
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(i -> ingredients.add(i));
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
-    }
-
-    @GetMapping
-    public String showDesignForm(Model model) {
-        model.addAttribute("design", new Taco());
         return "design";
     }
 
     @PostMapping
-    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors){
+    public String processDesign(@Valid Taco design, Errors errors){
         if(errors.hasErrors()){
             return "design";
         }
--- src/main/java/com/example/tacocloud/Models/Taco.java
+++ src/main/java/com/example/tacocloud/Models/Taco.java
@@ -4,14 +4,20 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    private Long id;
+
+    private Date createAt;
+
     @NotNull
     @Size(min = 5, message="Name must be at least 5 characters long")
     private String name;
+
     @NotNull
     @Size(min = 3,  message = "You must choose at least 3 ingredient")
     private List<String> ingredients;
