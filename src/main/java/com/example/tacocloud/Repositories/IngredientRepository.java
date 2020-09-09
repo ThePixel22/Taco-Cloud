1f01d96f9e32fd9cfe4c1bd861f7d26d0d9dd353
hungnh <hungnh@oneempower.com>
2020-09-09 17:53:12 +0700

    3.2 JPA - add repos

---
+++ src/main/java/com/example/tacocloud/Repositories/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.example.tacocloud.Repositories;
+
+import com.example.tacocloud.Models.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+}
